f5332795a5223b0abf4b4759086035cc92258277
ayushkumarOP <97456884+ayushkumarOP@users.noreply.github.com>
2024-07-06 15:48:31 +0530

    ayush - added (add book API)

--- Backend/controller/book.controller.js
+++ Backend/controller/book.controller.js
@@ -1,6 +1,24 @@
+// Import the necessary modules
 import Book from "../model/book.model.js";
+import cloudinary from '../middleware/cloudinary.js';
+import multer from 'multer';
+import fs from 'fs';
 
-export const getBook = async(req, res) => {
+import { v4 as uuidv4 } from 'uuid';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        const random = uuidv4(); 
+      cb(null, random+""+file.originalname )
+    }
+  })
+const upload = multer({ storage: storage })
+
+
+export const getBook = async (req, res) => {
     try {
         const book = await Book.find();
         res.status(200).json(book);
@@ -9,3 +27,29 @@ export const getBook = async(req, res) => {
         res.status(500).json(error);
     }
 };
+
+export const addBook = [upload.single('image'), async (req, res) => {
+        const { name, price, category, title } = req.body;
+        const x =  await cloudinary.uploader.upload(req.file.path)
+        const nami = req.body.name;
+        
+        const newBook = new Book({ 
+            name:name,
+            price:price,
+            category:category,
+            image: x.secure_url ,
+            title:title,
+        });
+
+        await newBook.save();
+
+        fs.unlink((req.file.path),function(err) {
+            if (err) console.log(err);
+            else {
+                console.log("\nDeleted file");
+            }
+        })
+
+        res.json({newBook})
+    }
+];
